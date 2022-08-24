6895674ac009d8d4438fc4d8deb7c7e112a5bbd8
Douglas Monteiro <douglasm.dev@gmail.com>
2022-08-24 18:52:55 -0300

    finish component contact

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,48 @@
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_2.jpg";
+import { MdEmail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
 import "./contact.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <Header title='Get In Touch' image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+        quos, architecto omnis voluptatum enim minus voluptas perferendis
+        consequatur quae nulla consequuntur adipisci aperiam, sed harum
+        illum cum? Animi, necessitatibus reprehenderit.
+      </Header>
+      <section className='contact'>
+        <container className='contact__container'>
+          <div className='contact__wrapper'>
+            <a
+              href='mailto:douglasmonteiro071@gmail.com'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              <MdEmail />
+            </a>
+            <a
+              href='http://m.me/douglas.monteiro.5220'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              <BsMessenger />
+            </a>
+            <a
+              href='https://wa.me/+5593992206317'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              <IoLogoWhatsapp />
+            </a>
+          </div>
+        </container>
+      </section>
+    </>
+  );
 };
 
 export default Contact;
