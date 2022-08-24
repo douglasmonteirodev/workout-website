896de6c6af8076b3ad50264eb1f53f32a51c451d
Douglas Monteiro <douglasm.dev@gmail.com>
2022-08-24 20:56:17 -0300

    finish project

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,7 +1,37 @@
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_4.jpg";
+import Card from "../../UI/Card";
+import { plans } from "../../data";
+
 import "./plans.css";
 
 const Plans = () => {
-  return <div>Plans</div>;
+  return (
+    <>
+      <Header title='Membership Plans' image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni eos
+        blanditiis quod.
+      </Header>
+      <section className='plans'>
+        <div className='container plans__container'>
+          {plans.map(({ id, name, desc, price, features }) => (
+            <Card key={id} className='plan'>
+              <h3>{name}</h3>
+              <small>{desc}</small>
+              <h1>{`$${price}`}</h1> <h2>/mo</h2>
+              <h4>Features</h4>
+              {features.map(({ feature, available }, index) => (
+                <p key={index} className={!available ? "disable" : ""}>
+                  {feature}
+                </p>
+              ))}
+              <button className='btn lg'>Choose Plan</button>
+            </Card>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Plans;
